a795f56acf5abb2e16cecf20e883e2a58820c00b
Dshafi <167286721+Dshafi@users.noreply.github.com>
2026-03-29 14:50:57 +0530

    Added log analyzer logic

--- log-analyzer/log_analyzer.py
+++ log-analyzer/log_analyzer.py
@@ -1,1 +1,19 @@
+def analyze_logs(file_path):
+    errors = 0
+    warnings = 0
 
+    with open(file_path, "r") as file:
+        for line in file:
+            if "ERROR" in line:
+                errors += 1
+            elif "WARNING" in line:
+                warnings += 1
+
+    print("Log Analysis Report")
+    print("-------------------")
+    print(f"Total Errors: {errors}")
+    print(f"Total Warnings: {warnings}")
+
+
+if __name__ == "__main__":
+    analyze_logs("log.txt")
